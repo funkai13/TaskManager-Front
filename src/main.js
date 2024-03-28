e9a6321980c2df79b9aeb437760de5a50aa41d46
funkai13 <evinssonmedrano@gmail.com>
2024-03-27 18:37:51 -0600

    change api route

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 
 window.axios = axios
 
-window.axios.defaults.baseURL = 'https://sea-lion-app-5j6li.ondigitalocean.app/api/'
+window.axios.defaults.baseURL = 'https://sea-lion-app-5j6li.ondigitalocean.app/'
 window.axios.defaults.headers.common['Accept'] = 'application/json'
 window.axios.defaults.headers.common['Content-Type'] = 'application/json'
 window.axios.defaults.headers.common['X-Requested-with'] = 'XMLHttpRequest'
